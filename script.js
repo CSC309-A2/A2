da976179066e53094b15a0543cd92ba0039bba9c
trinayeap <trina.yeapml@gmail.com>
2015-06-09 15:06:15 -0400

    Added start_game function

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function select_one(radio_button){
     if (this.__chk) 
         this.checked = false
 }
+
+function start_game(start_button){
+    var start_button = document.getElementById('start_button');
+    var game_page = document.getElementById('game_page');
+    var start_page = document.getElementById('start_page');
+    game_page.style.display = 'block';
+    start_page.style.display = 'none';
+}
+
+function pause_game(pause_button){
+    var start_button = document.getElementById('start_button');
+    var game_page = document.getElementById('game_page');
+    var start_page = document.getElementById('start_page');
+    game_page.style.display = 'none';
+    start_page.style.display = 'block';
+}
