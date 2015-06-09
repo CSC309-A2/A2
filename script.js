3277eeb5224ad3f1feea051a7ec8d1d5732f6cd2
trinayeap <trina.yeapml@gmail.com>
2015-06-09 17:00:44 -0400

    Implemented timer function

--- script.js
+++ script.js
@@ -12,9 +12,38 @@ function start_game(start_button){
 }
 
 function pause_game(pause_button){
-    var start_button = document.getElementById('start_button');
+    var elem = document.getElementById('pause_button');
+    if (elem.value=="PAUSE"){
+        elem.value = "RESUME"
+    } else{
+        elem.value = "PAUSE"
+    }
+}
+
+function countdownTimer(minutes) {
     var game_page = document.getElementById('game_page');
     var start_page = document.getElementById('start_page');
-    game_page.style.display = 'none';
-    start_page.style.display = 'block';
+    var seconds = 3;
+    var mins = minutes
+    function tick() {
+        var counter = document.getElementById("timer");
+        var current_minutes = mins-1
+        seconds--;
+        counter.innerHTML = 
+current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        if(seconds > 0) {
+            setTimeout(tick, 1000);
+        } else {
+            if(mins > 1){   
+               setTimeout(function () { countdown(mins - 1); }, 1000);     
+            } else {
+                setTimeout(function () {
+                    game_page.style.display = 'none';
+                    start_page.style.display = 'block';
+                }, 1000);
+            }
+        }
+    }
+    tick();
+        
 }
