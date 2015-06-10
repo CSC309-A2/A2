14148d127481d8dd2aa3ba27cc590fd58a463937
trinayeap <trina.yeapml@gmail.com>
2015-06-09 22:03:26 -0400

    Implemented pause function

--- script.js
+++ script.js
@@ -11,39 +11,40 @@ function start_game(start_button){
     start_page.style.display = 'none';
 }
 
+var t = 0;
+
 function pause_game(pause_button){
     var elem = document.getElementById('pause_button');
     if (elem.value=="PAUSE"){
-        elem.value = "RESUME"
-    } else{
-        elem.value = "PAUSE"
+        clearTimeout(t);
+        elem.value = "RESUME";
+    }
+    else {
+        elem.value = "PAUSE";
     }
 }
 
-function countdownTimer(minutes) {
+
+function countdownTimer(secs) {
     var game_page = document.getElementById('game_page');
     var start_page = document.getElementById('start_page');
-    var seconds = 3;
-    var mins = minutes
+    var seconds = secs;
+    //var mins = minutes;
     function tick() {
         var counter = document.getElementById("timer");
-        var current_minutes = mins-1
         seconds--;
         counter.innerHTML = 
-current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+"0" + ":" + (seconds < 10 ? "0" : "") + String(seconds);
         if(seconds > 0) {
-            setTimeout(tick, 1000);
-        } else {
-            if(mins > 1){   
-               setTimeout(function () { countdown(mins - 1); }, 1000);     
-            } else {
+            t = setTimeout(tick, 1000);
+        }
+        else {
                 setTimeout(function () {
                     game_page.style.display = 'none';
                     start_page.style.display = 'block';
                 }, 1000);
             }
         }
-    }
     tick();
-        
 }
+
